c551ead0367afc8ccc9d6afb6dcd7edf85cdab4c
Jordan Miller <jordan.j.miller@live.com>
2019-09-29 12:40:11 -0700

    Mvps done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelector('nav');
+//nav.prepend(document.createElement('a'));
+let navAnchors = document.querySelectorAll('a');
+navAnchors[0].textContent = 'Services';
+//navAnchors[0].style.color = 'green';
+navAnchors[1].textContent = 'Product';
+navAnchors[2].textContent = 'Vision';
+navAnchors[3].textContent = 'Features';
+navAnchors[4].textContent = 'About';
+navAnchors[5].textContent = 'Contact';
+
+let newA = document.createElement('a');
+newA.href = "#";
+newA.textContent = "Prepended";
+nav.prepend(newA);
+let newA2 = document.createElement('a');
+newA2.href = "#";
+newA2.textContent = "Appended";
+nav.append(newA2);
+
+navAnchors = document.querySelectorAll('a');
+navAnchors.forEach((anchor) => {anchor.style.color = 'green';} );
+
+const ctaH1 = document.querySelector('h1');
+ctaH1.style.whiteSpace = 'pre';
+ctaH1.textContent = 'DOM\n Is\n Awesome';
+const ctaButton = document.querySelector('button');
+ctaButton.textContent = 'Get Started';
+
+const ctaImg = document.getElementById("cta-img");
+ctaImg.src = "img/header-img.png";
+ctaImg.alt = "Image of a code snippet.";
+const midImg = document.getElementById("middle-img");
+midImg.src = "img/mid-page-accent.jpg";
+midImg.alt = "Image of code snippets across the screen";
+
+let h4s = document.querySelectorAll("h4");
+h4s[0].textContent = 'Features';
+h4s[1].textContent = 'About';
+h4s[2].textContent = 'Services';
+h4s[3].textContent = 'Product';
+h4s[4].textContent = 'Vision';
+h4s[5].textContent = 'Contact';
+
+let ps = document.querySelectorAll("p");
+ps[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[5].style.whiteSpace = 'pre';
+ps[5].textContent = '123 Way 456 Street\nSomewhere, USA';
+ps[6].textContent = '1 (888) 888-8888';
+ps[7].textContent = 'sales@greatidea.io';
+ps[8].textContent = 'Copyright Great Idea! 2018';
